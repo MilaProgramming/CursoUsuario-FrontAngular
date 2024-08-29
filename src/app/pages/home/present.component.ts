9f851514542c48477df1082401876086cd25a132
Mila <carivera14@espe.edu.ec>
2024-08-29 02:09:17 -0500

    Validacion multiples instancias

--- src/app/pages/home/present.component.ts
+++ src/app/pages/home/present.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { KeycloakService } from '../../services/keycloack.service'; // Adjust path if necessary
 
 @Component({
@@ -8,11 +9,32 @@ import { KeycloakService } from '../../services/keycloack.service'; // Adjust pa
   styleUrls: ['./present.component.css'] // Ensure correct path
 })
 export class PresentComponent {
-  constructor(private keycloakService: KeycloakService) {
+  private keycloakInitialized: boolean = false;
+
+  constructor(private keycloakService: KeycloakService, private router: Router) {
     console.log('PresentComponent initialized');
   }
 
-  login() {
-    this.keycloakService.init();
+  private async initializeKeycloak(): Promise<void> {
+    try {
+      if (!this.keycloakInitialized) {
+        await this.keycloakService.init();
+        this.keycloakInitialized = true;
+      }
+    } catch (error) {
+      console.error('Error initializing Keycloak:', error);
+    }
+  }
+
+  async login(): Promise<void> {
+    if (!this.keycloakInitialized) {
+      await this.initializeKeycloak();
+    }
+
+    if (this.keycloakService.isAuthenticated()) {
+      console.log('User is already authenticated');
+    } else {
+      this.keycloakService.init(); // Start the login process
+    }
   }
 }
